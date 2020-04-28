1a5db2b56da2996358ed101494bae527fb7f1b1f
Git-Project2000 <1586756482@qq.com>
2020-04-28 12:08:10 +0800

    创建文件 Student.java

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student{
+	private Integer id;
+	private String name;
+
+	public void setId(Integer id){
+		thid.id=id;
+	}
+
+	public Integer getId(){
+		return this.id;
+	}
+
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return this.name;
+	}
+
+
+}
+
